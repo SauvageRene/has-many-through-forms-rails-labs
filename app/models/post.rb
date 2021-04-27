d3890243335fe834139ed5b848139a825301be2f
Rene <renegarcia1864@gmail.com>
2021-04-26 21:22:41 -0500

    commit tree 9c9c778f031304640154188af1a0e0a99784457d

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,20 @@
 class Post < ActiveRecord::Base
   has_many :post_categories
+  #gives us methods example Post_categories, post_categories << , post_categories.delete 
+  # #post_category_ids=ids
+
   has_many :categories, through: :post_categories
+  #has_many_through gives us more methods example, 
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      if category_attribute["name"].present?
+        category = Category.find_or_create_by(category_attribute)
+        self.categories << category
+      end
+    end
+  end
 
 end
